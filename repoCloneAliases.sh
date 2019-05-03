ba1f0dc6abd57afe4733293c6f2e3e9676aa0122
Rami Rosen <ramirose@gmail.com>
2019-05-03 09:56:08 +0300

    add aliases for git cloning common repos
    Signed-off-by: Rami Rosen <ramirose@gmail.com>

---
+++ repoCloneAliases.sh
@@ -0,0 +1,19 @@
+#!/bin/bash 
+
+#Aliases for commont repositories
+
+alias gcllinux-next='git clone http://git.kernel.org/pub/scm/linux/kernel/git/next/linux-next.git'
+alias gclnet-next='git clone http://git.kernel.org/pub/scm/linux/kernel/git/davem/net-next.git'
+alias gcldpdk='git clone http://dpdk.org/git/dpdk'
+alias gclqemu='git clone https://github.com/qemu/qemu.git'
+alias gclspdk='git clone https://github.com/spdk/spdk; cd spdk; git submodule update --init'
+
+alias gcldts='git clone http://dpdk.org/git/tools/dts'
+alias gclurdma='git clone https://github.com/zrlio/urdma.git'
+alias gclovs='git clone https://github.com/openvswitch/ovs.git'
+alias gclpktgen-dpdk='git clone git://dpdk.org/apps/pktgen-dpdk'
+alias gcliproute2='git clone git://git.kernel.org/pub/scm/network/iproute2/iproute2.git'
+alias gcliproute2-next='git://git.kernel.org/pub/scm/network/iproute2/iproute2-next.git'
+alias gclrdma-core='git clone https://github.com/linux-rdma/rdma-core.git'
+alias gclethtool='git clone git://git.kernel.org/pub/scm/network/ethtool/ethtool.git'
+
